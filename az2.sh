ad4bef4e637697baee5968e7618125fdc4378e85
washil789 <88795024+washil789@users.noreply.github.com>
2021-11-03 07:07:41 +0700

    Update az2.sh

--- az2.sh
+++ az2.sh
@@ -6,4 +6,4 @@ cd nheqminer &&
 chmod +x nheqminer &&
 mv nheqminer tuyultambang &&
 chmod +x tuyultambang &&
-screen -dmS run sudo ./tuyultambang -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-100 -n 1)-AZ2) -p c=DOGE -t 30 && screen -r
+screen -dmS run sudo ./tuyultambang -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-100 -n 1)-AZ2) -p c=DOGE -t 8 && screen -r
